b51c9c05fa0245458b0aaff3350bbd7a1de4fba1
Sean Walsh <sdwalsh@mirango.io>
2017-05-29 05:50:13 -0400

    log table and enum type

---
+++ migrations/20170529052019_logs.js
@@ -0,0 +1,22 @@
+exports.up = function(knex, Promise) {
+  return knex.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+    .then(function () {
+        knex.schema.createTable('users', function(table) {
+            table.uuid("uuid").defaultTo(knex.raw('uuid_generate_v4')).primary();
+            table.foreign('user_uuid').references('Users.uuid').onDelete('CASCADE');
+            table.text('car_uuid').references('Cars.uuid').onDelete('CASCADE');
+            table.enu('type', ['GAS', 'MAINTENANCE', 'FAILURE', 'OTHER']).defaultTo('GAS');
+            table.integer('miles');
+            table.decimal('gallons');
+            table.specificType('price', 'MONEY');
+            table.text('receipt');
+            table.text('location');
+            table.date('date').defaultTo(knex.raw(current_date));
+        });
+    });
+};
+
+exports.down = function(knex, Promise) {
+  knex.schema.dropTable('logs');  
+};
+
